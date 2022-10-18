92f916cdcc6f5402d65d45c3fb54ed97f50c1dc8
AlejoPrzHub <josealeprz@gmail.com>
2022-10-18 18:46:53 +0200

    "v0.0.2"

--- IMDB/main.js
+++ IMDB/main.js
@@ -1,4 +1,5 @@
 import { Professional } from "./proyecto.js"
+import {Movie} from "./proyecto.js"
 
 let webDev = new Professional("jose",27,"men","70kg","186cm","black","green","white","no","venezuela",0,"web dev");
 let astronomo = new Professional("juan",35,"men","80kg","180cm","grey","brown","black","no","colombia",1,"astronomo");
@@ -7,3 +8,8 @@ let actriz = new Professional("julieta",22,"women","60kg","174cm","brown","blue"
 webDev.printAll();
 astronomo.printAll();
 actriz.printAll();
+
+
+let pelicula = new Movie("La Casa Embrujada",2022,"Spain","Horror");
+
+pelicula.printAll();
